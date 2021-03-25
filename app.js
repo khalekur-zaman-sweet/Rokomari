b9d5877b1b4378afb1647fe85f9238aee546eaa6
Khalekur Zaman Sweet <sweetislam1626@gmail.com>
2021-03-26 00:17:18 +0600

    3-26-2021

---
+++ app.js
@@ -0,0 +1,17 @@
+
+var typed = new Typed('#search', {
+    
+    strings: ['Search by Publisher (ex. মাওলা ব্রাদার্স , Harpercollins)', 
+            'Search by (ex. ক্রীতদাসের হাসি , The Power Of Habit.)',
+            'Search by Authors (ex. চমক হাসান ,J.K. Rowling)'
+        ],
+        startDelay: 1200,
+        typespeed: 200,
+        backspeed: 20,
+        backDelay: 500,
+        attr: 'placeholder',
+        bindInputFocusEvents: true,
+        loop: true,
+        
+
+  });
